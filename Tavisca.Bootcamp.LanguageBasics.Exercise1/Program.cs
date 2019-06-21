badc6aacf71139e5cbf7c6c18f0a364d443b8ac5
tavisca-asmita <51910971+tavisca-asmita@users.noreply.github.com>
2019-06-21 19:21:16 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,6 +23,73 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
+            char[] delimit = { '*', '=' };
+            string[] var = equation.Split(delimit);
+            int a, b, c;
+            string s;
+            //A contains ?
+            if (var[0].Contains('?'))
+            {
+                b = Convert.ToInt32(var[1]);
+                c = Convert.ToInt32(var[2]);
+                if(c % b != 0)
+                {
+                    return -1;
+                }
+                else
+                {
+                    a = c / b;
+                    s = a.ToString();
+                    if (s.Length == var[0].Length)
+                    {
+                        return Convert.ToInt32(s[var[0].IndexOf('?')].ToString());
+                    }
+                    else
+                        return -1;
+                }               
+
+            }
+
+            //B contains ?
+            if (var[1].Contains('?'))
+            {
+                a = Convert.ToInt32(var[0]);
+                c = Convert.ToInt32(var[2]);
+                if (c % a != 0)
+                {
+                    return -1;
+                }
+                else
+                {
+                    b = c / a;
+                    s = b.ToString();
+                    if (s.Length == var[1].Length)
+                    {
+                        return Convert.ToInt32(s[var[1].IndexOf('?')].ToString());
+                    }
+                    else
+                        return -1;
+                    
+                }
+
+            }
+
+            //C contains ?
+            else
+            {
+                a = Convert.ToInt32(var[0]);
+                b = Convert.ToInt32(var[1]);
+                c = a * b;
+                s = c.ToString();
+                if (s.Length == var[2].Length)
+                {
+                    return Convert.ToInt32(s[var[2].IndexOf('?')].ToString());
+                }
+                else
+                    return -1;
+
+
+            }
             throw new NotImplementedException();
         }
     }
